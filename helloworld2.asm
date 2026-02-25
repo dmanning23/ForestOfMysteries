19b84de20dcfcf6767157f6ea72d1581d47e17b2
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2026-02-24 23:14:18 -0600

    fixed tile engine

---
+++ helloworld2.asm
@@ -0,0 +1,159 @@
+
+    processor 6502
+    include "vcs.h"
+    include "macro.h"
+    include "xmacro.h"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Constants segment
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    include "Constants.dasm"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Variables segment
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    seg.u Variables
+    org $80
+
+    include "Variables.dasm"
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Code segment
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    seg Code
+    org $f000
+
+Start
+    CLEAN_START
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; Startup code
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+NextFrame
+
+    ; 1 + 3 lines of VSYNC
+    VERTICAL_SYNC
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; 35 lines of underscan
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    TIMER_SETUP 35
+
+    lda #192
+    sta COLUPF
+
+    lda #70
+    sta playerYPos
+
+    TIMER_WAIT
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; 192 lines of frame
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+LinesOfFrame
+
+   ldy #192
+    ldx #192
+
+.lvscan
+
+    ; even lines: draw the bitmap background
+    sta WSYNC
+    lda PFBitmap0,Y
+    sta PF0
+    lda PFBitmap1,Y
+    sta PF1
+    lda PFBitmap2,Y
+    sta PF2
+    nop
+    nop
+    nop
+    lda PFBitmap3,Y
+    sta PF0
+    lda PFBitmap4,Y
+    sta PF1
+    lda PFBitmap5,Y
+    sta PF2
+
+    dex
+
+    sta WSYNC
+    lda #0
+    sta PF0
+    sta PF1
+    sta PF2
+
+    ; odd lines: draw the player sprite (inlined - no JSR/RTS overhead)
+    sty temp
+    
+     txa                     ; X = current scanline
+     sec
+     sbc playerYPos          ; local coordinate
+     cmp #SPRITE_HEIGHT      ; in sprite?
+     bcc InSprite           ; yes
+     lda #0                  ; no, use row 0 (transparent)
+InSprite
+     tay                     ; sprite row -> Y
+;     lda (spritePtr),y       ; lookup sprite pattern
+     
+     sta GRP0                ; write sprite bitmap
+;     lda (spriteColorPtr),y  ; lookup sprite color
+     sta COLUP0              ; write color
+    ldy temp               ; restore bitmap row index
+
+    dex
+    txa
+    and #$03
+    bne .notDivisible_by_4
+
+    dey
+
+.notDivisible_by_4
+
+    cpx #0 ;sets the Z flag based on X
+    bne .lvscan
+
+    ldy #0  ; restore Y so ScreenWraparound/CheckInput index player 0 correctly
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; 29 lines of overscan
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+OverScan
+    TIMER_SETUP 29
+    TIMER_WAIT
+
+    ; total = 262 lines, go to next frame
+    jmp NextFrame
+
+    include "Assets/Graphics/helloworld.asm"
+
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; Epilogue
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    org $fffc
+    .word Start ;reset vector
+    .word Start ;BRK vector
