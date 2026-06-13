179d062b7f95a4086cfa20e8db894db2fb00abe8
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2026-06-13 18:57:42 -0500

    added hiding animation

--- GameplayScreen.asm
+++ GameplayScreen.asm
@@ -19,6 +19,24 @@ GameplayScreenLogic subroutine
     tay
     jsr PerformScreenLogic
 
+    ;Check if the player is hiding
+    bit CXP0FB
+    bpl .NoCollion
+    ;yes, use eyball sprite
+    lda #<Frame2        ; load low byte of Frame0 address
+    sta spritePtr
+    lda #>Frame2        ; load high byte of Frame0 address
+    sta spritePtr+1
+
+    lda #<ColorFrame0        ; load low byte of Frame0 address
+    sta spriteColorPtr
+    lda #>ColorFrame0        ; load high byte of Frame0 address
+    sta spriteColorPtr+1
+    jmp .DoneCollision
+
+.NoCollion
+    ;no, use walking sprite
+
     lda #<Frame0        ; load low byte of Frame0 address
     sta spritePtr
     lda #>Frame0        ; load high byte of Frame0 address
@@ -29,6 +47,8 @@ GameplayScreenLogic subroutine
     lda #>ColorFrame0        ; load high byte of Frame0 address
     sta spriteColorPtr+1
 
+.DoneCollision
+
     ;Set the x position of the player
     lda playerXPos
     ldx #0
@@ -37,21 +57,23 @@ GameplayScreenLogic subroutine
     sta WSYNC	; sync w/ scanline
     sta HMOVE	; apply fine offsets
 
+    sta CXCLR ;clear collision
+
     ;Update the sound engine
     BANK_SWITCH 4,UpdateWhisperingTrees
 
 DoneUpdateGameplayScreenLogic
 
-    ;TEST: Show the win screen if the button is down
-    bit INPT4
-    bmi .skipButton
+    ; ;TEST: Show the win screen if the button is down
+    ; bit INPT4
+    ; bmi .skipButton
 
-    ;change to gameplay mode
-    lda #2
-    sta gameMode
+    ; ;change to gameplay mode
+    ; lda #2
+    ; sta gameMode
 
-    ;Start the music
-    BANK_SWITCH 4,StartIntoTheLight
+    ; ;Start the music
+    ; BANK_SWITCH 4,StartIntoTheLight
 
 .skipButton
 
